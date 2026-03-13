41e14b89b79de235a37f3f2a0151a6be2458a3e2
Darin Kotter <darin.kotter@gmail.com>
2026-03-13 09:10:20 -0600

    Add support for the logo path field

--- includes/Provider/AzureOpenAIProvider.php
+++ includes/Provider/AzureOpenAIProvider.php
@@ -96,6 +96,11 @@ class AzureOpenAIProvider extends AbstractApiProvider {
 			}
 		}
 
+		// Provider logo path support was added in 1.3.0.
+		if ( version_compare( AiClient::VERSION, '1.3.0', '>=' ) ) {
+			$provider_meta[] = __DIR__ . '/logo.svg';
+		}
+
 		return new ProviderMetadata( ...$provider_meta );
 	}
 
